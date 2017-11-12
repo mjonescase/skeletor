001546f6ec3f7e1a4db46cb2d5ca7c4de4e953ed
Mike Jones <mwj12@case.edu>
2017-11-12 18:16:18 -0500

    modify server to support web sockets

--- wiki.go
+++ wiki.go
@@ -1,14 +1,80 @@
 package main
 import (
-       "fmt"
        "net/http"
+       "log"
+
+       "github.com/gorilla/websocket"
 )
 
-func handler(writer http.ResponseWriter, request *http.Request) {
-     fmt.Fprintf(writer, "Hi there, I love %s!", request.URL.Path[1:])
+var clients = make(map[*websocket.Conn]bool) // connected clients
+var broadcast = make(chan Message)	     // broadcast channel
+
+// configure the upgrader
+var upgrader = websocket.Upgrader{}
+
+// define our message object
+type Message struct {
+     Email    string `json:"email"`
+     Username string `json:"username"`
+     Message  string `json:"message"`
+}
+
+func handleConnections(writer http.ResponseWriter, request *http.Request) {
+     ws, err := upgrader.Upgrade(writer, request, nil)
+     if err != nil {
+     	log.Fatal(err)
+     }
+
+     // Make sure we close the connection when the function returns
+     defer ws.Close()
+
+     // Register our new client
+     clients[ws] = true
+
+     for {
+     	 var msg Message
+	 // Read in a new message as JSON and map it to a Message object
+	 err := ws.ReadJSON(&msg)
+	 if err != nil {
+	    log.Printf("error: %v", err)
+	    delete(clients, ws)
+	    break
+	 }
+
+	 // Send the newly received message to the broadcast channel
+	 broadcast <- msg
+     }
+}
+
+func handleMessages() {
+     for {
+     	 // Grab the next message from the broadcast channel
+	 msg := <-broadcast
+	 // Send it out to every client that is currently connected
+	 for client := range clients {
+	     err := client.WriteJSON(msg)
+	     if err != nil {
+	     	log.Printf("error: %v", err)
+		client.Close()
+		delete(clients, client)
+	     }
+	 }
+     }
 }
 
 func main() {
-     http.HandleFunc("/", handler)
-     http.ListenAndServe(":5000", nil)
+     // Create a simple file server
+     fs := http.FileServer(http.Dir("../public"))
+     http.Handle("/", fs)
+     http.HandleFunc("/ws", handleConnections)
+
+     // Start listening for incoming chat messages
+     go handleMessages()
+
+     // Start the server on localhost port 5000 and log any errors
+     log.Println("http server started on :5000")
+     err := http.ListenAndServe(":5000", nil)
+     if err != nil {
+     	log.Fatal("ListenAndServe: ", err)
+     }
 }
