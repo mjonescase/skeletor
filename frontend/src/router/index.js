193a1694996eaba4a042adee8f2b515a7f77dcb7
Ali Orlando <aorlando@onshift.com>
2017-11-16 13:35:37 -0500

    add the login page route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Login from '../pages/Login.vue';
 import ChatRoomPage from '../pages/ChatRoomPage.vue';
 
 Vue.use(Router)
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Chat',
       component: ChatRoomPage
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
