ce3e5afe5e3c268055b20727a570af145a429f78
Ali Orlando <aorlando@onshift.com>
2017-11-16 13:14:41 -0500

    move app to pages and set up route

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,20 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 new Vue({
   el: '#app',
-  data: {
-    ws: null, // Our websocket
-    newMsg: '', // Holds new messages to be sent to the server
-    messages: [], // A running list of chat messages displayed on the screen
-    mobilenumber: null,
-    username: null,
-    firstname: null,
-    lastname: null,
-    email: null,
-    title: null,
-    password: null,
-    joined: false // True if email and username have been filled in
-  },
+  router,
   render: h => h(App)
 })
