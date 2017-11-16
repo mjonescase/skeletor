849d2be3249b069045df636ad0ecce2d85354608
Ali Orlando <aorlando@onshift.com>
2017-11-16 14:37:29 -0500

    add some overriding styles to app.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+  <router-view></router-view>
 </template>
 
 <script>
@@ -9,3 +7,22 @@
     name: 'app'
   }
 </script>
+
+<style>
+  body {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+  main {
+    flex: 1 0 auto;
+  }
+
+  .flex-1-auto {
+    flex: 1 1 auto;
+  }
+  .mw-75 {
+    max-width: 75%;
+  }
+</style>
