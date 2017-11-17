8d5054501ca8b448b28a1cfad4002ac6bb482809
Ali Orlando <aorlando@onshift.com>
2017-11-17 09:09:44 -0500

    app change

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,14 +2,18 @@
   <div class="h-100">
     <navbar></navbar>
     <router-view></router-view>
+    <footer class="page-footer"></footer>
   </div>
 </template>
 
 <script>
   import Navbar from './components/Navbar.vue';
+
   export default {
     name: 'app',
-    components: { Navbar }
+    components: {
+      Navbar
+    }
   }
 </script>
 
